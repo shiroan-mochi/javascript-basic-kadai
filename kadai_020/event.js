8b0ee3e30b71f988fe3a83175d8dc8c76d18b69c
shiroan-mochi <155418531+shiroan-mochi@users.noreply.github.com>
2024-03-11 21:21:17 +0900

    kadai_002再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,3 +1,13 @@
-document.getElementById("btn").onclick = function(){
-document.getElementById("text").innerHTML = "ボタンをクリックしました"
-}
+//再提出になった方
+// document.getElementById("btn").onclick = function(){
+// document.getElementById("text").innerHTML = "ボタンをクリックしました"
+// }
+
+
+const clickBtn = document.getElementById('btn');
+const textMessage = document.getElementById('text');
+
+clickBtn.addEventListener('click',()=>{
+  document.createElement('text')
+  textMessage.textContent='ボタンをクリックしました';
+});
